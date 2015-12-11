cae8289c9cf0298c6fcff384f83671099312cb77
gmorklla <gmorklla@gmail.com>
2015-12-11 15:04:05 -0600

    Primer commit: Header, footer, artículos y detalle dinámicos

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+			
+			<section id="main-content">
+				
+				<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+					<article class="post resume">
+						<header class="post-title">
+							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							<small class="meta"><?php the_time(get_option('date_format')); ?> &bull; <?php the_category(', '); ?></small>
+						</header>
+						<div class="post-content">
+							<?php the_excerpt(); ?>
+							<a href="<?php the_permalink(); ?>" class="readmore"><?php _e('Seguir leyendo &rarr;', 'apk'); ?></a>
+						</div>
+					</article>	<!-- article -->
+
+				<?php endwhile; else : ?>
+
+					<article class="post resume">
+						<header class="post-title">
+							<h2><?php _e('No hay contenidos disponibles', 'apk'); ?></h2>
+						</header>
+						<div class="post-content">
+							<p><?php _e('No hay contenidos que correspondan con esta página, por favor realiza una búsqueda para encontrar lo que deseas ver:', 'apk'); ?></p>
+							<?php get_search_form(); ?>
+						</div>
+					</article>	<!-- article -->				
+
+				<?php endif; ?>
+				
+				<?php if( get_next_posts_link() || get_previous_posts_link() ) { ?>
+
+				<div class="posts-nav cf">
+					<?php next_posts_link( __('&larr; Entradas pasadas', 'apk') ); ?>
+					<?php previous_posts_link( __('Entradas recientes &rarr;', 'apk') ); ?>
+				</div>
+
+				<?php } ?>
+				
+			</section><!-- /#main-content -->
+			
+<?php get_sidebar(); ?>			
+
+<?php get_footer(); ?>			
+			
---
+++ sidebar.php
@@ -0,0 +1,12 @@
+			<aside id="sidebar">
+				
+				<?php if( is_active_sidebar('main_sidebar') ) {
+					dynamic_sidebar('main_sidebar');
+				} else { ?>
+					<div class="widget">
+						<h3 class="widget-title"><?php _e('Buscar', 'apk'); ?></h3>
+						<?php get_search_form(); ?>
+					</div>
+				<?php } ?>
+
+			</aside><!-- /#sidebar -->
